7d8145cd427bc90dafab9c7d63da498b94f8e16a
TinaRani2003 <160511456+TinaRani2003@users.noreply.github.com>
2024-06-18 00:04:34 +0530

    text commit

--- app/src/main/java/com/example/my_application/MainActivity.kt
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -4,19 +4,16 @@ package com.example.my_application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
